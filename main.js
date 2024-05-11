a666452dd52df28d19d9a97b606ae154ea0dd340
manihustler03 <manihustler03@gmail.com>
2024-05-11 14:34:25 +0530

    route structure

--- main.js
+++ main.js
@@ -6,6 +6,29 @@ app.get('/',(req,res)=>{
     res.json({msg:"Hello Students!"})
 })
 
+// CRUD Functionallity of movies
+
+// C - For Creating movie
+
+app.post('/movies',()=>{
+
+})
+
+// R-For Reading movie
+app.get('/movies',()=>{
+
+})
+
+// U-For Updating movie
+app.put('/movie:id',()=>{
+
+})
+
+// D-For Deleting movie
+app.delete('/movie:id',()=>{
+    
+})
+
 app.listen(PORT, ()=>{
     console.log(`The Server is running at http://localhost:${PORT}`);
 })
